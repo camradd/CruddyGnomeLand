7936e2195171cfd6214dffae820e90c63adcff38
Avinoam Henig <avhenig@gmail.com>
2015-04-28 15:50:18 -0400

    fix conflicts

--- World.py
+++ World.py
@@ -1,11 +1,17 @@
+class Tile:
+
+    def __init__(self):
+        pass
+
 class Food:
 
     '''
-    Food object. Has
+    Food object. Has variable nutrition value, which represents the amount of
+    health increase it causes.
     '''
 
     def __init__(self):
-        self.value = .2
+        self.affect = 0.2
 
     def __repr__(self):
         return "f"
@@ -13,11 +19,32 @@ class Food:
 class Tree:
 
     '''
-    Tree object, in world. Carries variable damage value.
+    Tree object, in world. Carries a damage value.
     '''
 
     def __init__(self):
-        self.value = .5
+        self.affect = -0.2
 
     def __repr__(self):
         return "T"
+
+class World:
+
+    tileClasses = [Tile, Food, Tree]
+    tileWeights = [0.5,  0.2,  0.3 ]
+
+    def __init__(self, width = 100, height = 100):
+        self.tiles = [[self.createTile() for x in range(width)] for y in range(height)]
+
+    def createTile(self):
+        tileClass = self._weightedChoice(tileClasses, )
+
+    def _weightedChoice(self, elements, weights):
+        randomNumber = random.uniform(0, sum(weights))
+        w = 0
+        for i in range(len(weights)):
+            w += weights[i]
+            if w > randomNumber:
+                return elements[i]
+        # all weights are 0
+        return random.choice(elements)
