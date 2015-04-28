d2cf639025b42c72ad06aa6cc9ebdf42bb98ed15
Avinoam Henig <avhenig@gmail.com>
2015-04-28 14:43:44 -0400

    create World.py

--- Creature.py
+++ Creature.py
@@ -72,28 +72,3 @@ class Creature:
         #setWeight(self, fromName, fromPos, toName, toPos, value)
         ## set EvalNet
         self.evalNet.setWeight("input", i, "hidden", j, self.genome[i])
-
-class Food:
-
-    '''
-    Food object. Has variable nutrition value, which is normally positive, but
-    can be negative.
-    '''
-
-    def __init__(self):
-        self.value = random.uniform(-1,10) #can be "posionous"
-
-    def __repr__(self):
-        return "f"
-
-class Tree:
-
-    '''
-    Tree object, in world. Carries variable damage value.
-    '''
-
-    def __init__(self):
-        self.value = random.uniform(-5,0)
-
-    def __repr__(self):
-        return "T"
