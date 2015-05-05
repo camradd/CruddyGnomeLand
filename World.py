d1c230dd476324f26bff343fce5e692e839c0483
OrbisTertius-Uqbar <crudd@gm.slc.edu>
2015-05-05 12:26:58 -0400

    added reproduction methods

--- World.py
+++ World.py
@@ -55,3 +55,35 @@ class World:
                 return elements[i]
         # all weights are 0
         return random.choice(elements)
+
+'''
+ Are actions handled in World class? We can move this to the Creature, but it
+ made most sense to me here, based on what we have now. Here is a reproduction
+ method that can handle any number of parents.
+'''
+    def reproduction(parents):
+        parents = [p.genome for p in parents]
+        if len(parents) == 1:
+            return Creature(mutation(parents[0]))
+        else:
+            while len(parents) > 1:
+                print "loopin"
+                p1 = random.choice(parents)
+                parents.remove(p1)
+                p2 = random.choice(parents)
+                parents.remove(p2)
+                parents.append(crossover(p1,p2))
+            return Creature(mutation(parents[0]))
+
+    def mutation(genome, rate = .3):
+        return [
+            g + random.uniform(-.5,.5) if \
+            random.uniform(0,1) < rate else g for g in genome
+        ]
+
+    def crossover(mateGenome, partnerGenome):
+        pivot = random.choice(range(len(mateGenome)))
+        return [
+            mateGenome[i] if i < pivot else \
+            partnerGenome[i] for i in range(len(mateGenome))
+        ]
