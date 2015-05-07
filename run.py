89792f551cb0d9bee31b468922dc4869cca14512
OrbisTertius-Uqbar <crudd@gm.slc.edu>
2015-05-06 22:00:13 -0400

    put window code in method
    sorry for all the updates…

--- run.py
+++ run.py
@@ -3,68 +3,70 @@ import TileObject, World, Creature, pyglet
 f = None
 data = None
 
-SIZE_X = 75
-SIZE_Y = 45
-tileSize = 16
-world = World.World(SIZE_X, SIZE_Y)
+def newWorld():
+    SIZE_X = 75
+    SIZE_Y = 45
+    tileSize = 16
+    world = World.World(SIZE_X, SIZE_Y)
 
-window = pyglet.window.Window(
-    width  = SIZE_X * tileSize,
-    height = SIZE_Y * tileSize
-)
-
-
-def makeTileImg(path):
-    img = pyglet.resource.image(path)
-    img.width = tileSize
-    img.height = tileSize
-    return img
-
-batch = pyglet.graphics.Batch()
-tileImages = {
-    TileObject.Tree:       makeTileImg('img/tree.png'),
-    TileObject.Food:       makeTileImg('img/food.png'),
-    TileObject.TileObject: makeTileImg('img/tile.png'),
-    Creature.Creature:     makeTileImg('img/creature.png')
-}
-sprites = [
-    [pyglet.sprite.Sprite(tileImages[TileObject.TileObject],
-                            col*tileSize, row*tileSize, batch=batch)
-        for col in range(SIZE_X)]
-    for row in range(SIZE_Y)
-]
+    window = pyglet.window.Window(
+        width  = SIZE_X * tileSize,
+        height = SIZE_Y * tileSize
+    )
 
 
+    def makeTileImg(path):
+        img = pyglet.resource.image(path)
+        img.width = tileSize
+        img.height = tileSize
+        return img
 
-@window.event
-def on_draw():
-    setSpriteImages()
-    batch.draw()
-    getCurrentState().draw()
+    batch = pyglet.graphics.Batch()
+    tileImages = {
+        TileObject.Tree:       makeTileImg('img/tree.png'),
+        TileObject.Food:       makeTileImg('img/food.png'),
+        TileObject.TileObject: makeTileImg('img/tile.png'),
+        Creature.Creature:     makeTileImg('img/creature.png')
+    }
+    sprites = [
+        [pyglet.sprite.Sprite(tileImages[TileObject.TileObject],
+                                col*tileSize, row*tileSize, batch=batch)
+            for col in range(SIZE_X)]
+        for row in range(SIZE_Y)
+    ]
 
-def getCurrentState():
-    return pyglet.text.Label('time step: %d  born: %d  dead: %d  alive: %d' %(world.time, world.born,
-                              world.dead, world.alive),
-                              font_name='mono',
-                              font_size= 10,
-                              x=window.width / 100, y=window.height / 100,
-                              anchor_x='left', anchor_y='bottom')
 
-def setSpriteImages():
-    for row in range(SIZE_Y):
-        for col in range(SIZE_X):
-            tileObject = world.tiles[row][col].visibleTileObject()
-            img = tileImages[tileObject.__class__]
-            sprites[row][col].image = img
 
-def step(dx):
-    world.step()
+    @window.event
+    def on_draw():
+        setSpriteImages()
+        batch.draw()
+        getCurrentState().draw()
 
-    if world.saveData == 'tstep':
-            data = "%d %d %d %d\n" %(world.time, world.born, world.dead, world.alive)
-            f.write(data)
+    def getCurrentState():
+        return pyglet.text.Label('time step: %d  born: %d  dead: %d  alive: %d' %(world.time, world.born,
+                                  world.dead, world.alive),
+                                  font_name='mono',
+                                  font_size= 10,
+                                  x=window.width / 100, y=window.height / 100,
+                                  anchor_x='left', anchor_y='bottom')
 
-pyglet.clock.schedule_interval(step, 0.01)
+    def setSpriteImages():
+        for row in range(SIZE_Y):
+            for col in range(SIZE_X):
+                tileObject = world.tiles[row][col].visibleTileObject()
+                img = tileImages[tileObject.__class__]
+                sprites[row][col].image = img
+
+    def step(dx):
+        world.step()
+
+        if world.saveData == 'tstep':
+                data = "%d %d %d %d\n" %(world.time, world.born, world.dead, world.alive)
+                f.write(data)
+
+    pyglet.clock.schedule_interval(step, 0.01)
+
 
 def main():
     print "To begin simulation type 'run'."
@@ -77,6 +79,7 @@ def main():
         commandLine = raw_input(">>> ")
 
         if commandLine == 'run':
+            newWorld()
             pyglet.app.run()
 
         if commandLine == 'create file':
